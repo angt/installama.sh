990adf0db75d8b6605cf340089a84bbf1a85f4ec
Adrien Gallouët <angt@huggingface.co>
2025-12-05 13:11:42 +0100

    Propagate errors from cmake calls in build.cmake
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- build.cmake
+++ build.cmake
@@ -11,7 +11,13 @@ endif()
 foreach(i RANGE 0 ${STOP})
     string(JSON PRESET_NAME GET "${CONTENTS}" workflowPresets ${i} name)
     if(PRESET_NAME MATCHES "${FILTER}")
-        execute_process(COMMAND ${CMAKE_COMMAND} --workflow --preset ${PRESET_NAME})
-        execute_process(COMMAND ${CMAKE_COMMAND} --build --preset ${PRESET_NAME} --target clean)
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} --workflow --preset ${PRESET_NAME}
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} --build --preset ${PRESET_NAME} --target clean
+            COMMAND_ERROR_IS_FATAL ANY
+        )
     endif()
 endforeach()
