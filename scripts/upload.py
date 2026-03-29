0631b820a598009ba58c8b0e7629b746b817f828
Adrien Gallouët <adrien@gallouet.fr>
2026-03-29 20:52:02 +0200

    Still need scripts/upload.py...
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,46 +1,30 @@
 import os
+import random
+import sys
 import time
 from huggingface_hub import HfApi, utils
 from functools import wraps
 
 api = HfApi()
-repo_id = os.environ.get("HF_REPO")
-
-if not repo_id:
-    raise ValueError("HF_REPO environment variable is not set")
 
 def retry(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        for i in reversed(range(10)):
+        for i in reversed(range(60)):
             try:
                 return func(*args, **kwargs)
             except Exception as e:
                 if not i:
                     raise
                 print(f"\n[RETRY] {type(e).__name__}: {e}\n")
-                time.sleep(30)
+                time.sleep(random.uniform(30, 90))
     return wrapper
 
 @retry
-def upload_folder():
-    api.upload_folder(
-        repo_id=repo_id,
-        folder_path="output",
-        repo_type="dataset",
-        commit_message="Update",
-        revision="latest"
-    )
+def upload(dst):
+    api.sync_bucket("output", f"hf://buckets/{dst}")
 
 utils.disable_progress_bars()
 
-try:
-    api.create_branch(
-        repo_id=repo_id,
-        repo_type="dataset",
-        branch="latest"
-    )
-except Exception:
-    pass
-
-upload_folder()
+if len(sys.argv) == 2:
+    upload(sys.argv[1])
