4ec347298e337b44d5b38ebae33d62c5ea804c9e
Adrien Gallouët <adrien@gallouet.fr>
2026-05-05 15:58:24 +0000

    Add installama_link_win32_shim
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- win32/shim.cmake
+++ win32/shim.cmake
@@ -1,16 +1,18 @@
 set(_shim_dir "${CMAKE_CURRENT_LIST_DIR}")
 
-function(installama_add_win32_shim target)
-    if(NOT INSTALLAMA_OS STREQUAL "windows")
-        return()
-    endif()
-
+if(INSTALLAMA_OS STREQUAL "windows")
     add_library(win32_shim STATIC "${_shim_dir}/shim.c")
 
     target_compile_options(win32_shim PRIVATE
         -Wno-unused-function
     )
-    target_link_libraries(${target} PRIVATE
-        -Wl,--whole-archive win32_shim -Wl,--no-whole-archive
-    )
-endfunction()
+
+    function(installama_link_win32_shim target)
+        target_link_libraries(${target} PRIVATE
+            -Wl,--whole-archive win32_shim -Wl,--no-whole-archive
+        )
+    endfunction()
+else()
+    function(installama_link_win32_shim target)
+    endfunction()
+endif()
